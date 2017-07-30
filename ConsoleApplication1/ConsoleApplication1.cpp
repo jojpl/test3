52cacdc711581ef06d80846fb5242143ae5c320f
D <D@D.com>
2017-07-31 00:50:31 +0400

    123

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello!!!!";
+    return 0;
+}
+
